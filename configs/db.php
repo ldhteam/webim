2514dc52c33840fb6bbb0b5c7ed365ab24880952
lidonghui <donghui_li@evetown.com.cn>
2017-12-07 11:45:39 +0800

    1

--- configs/db.php
+++ configs/db.php
@@ -1,13 +1,13 @@
 <?php
 $db['master'] = array(
     'type'       => Swoole\Database::TYPE_MYSQLi,
-    'host'       => "127.0.0.1",
+    'host'       => "115.28.240.188",
     'port'       => 3306,
     'dbms'       => 'mysql',
     'engine'     => 'MyISAM',
-    'user'       => "root",
-    'passwd'     => "root",
-    'name'       => "webim",
+    'user'       => "huitang",
+    'passwd'     => "Evt_huitang2015",
+    'name'       => "swim",
     'charset'    => "utf8",
     'setname'    => true,
     'persistent' => false, //MySQL长连接
